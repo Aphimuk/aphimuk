f2df8a74e82d362c42673b2c9ea35846e3ce9508
Aphimuk <66010914024@msu.ac.th>
2026-01-28 16:37:28 +0700

    a

--- e/e/connectdb.php
+++ e/e/connectdb.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pwd = "$mB#loPWzL3h";
+$pwd = "66010914005";
 $db = "4024db";
 $conn = mysqli_connect($host,$user,$pwd,$db) or die ("เชื่อต่อล้มเหลว");
 mysqli_query($conn, "SET NAMES utf8");
