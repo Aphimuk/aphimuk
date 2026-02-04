26d072679f94fb17506daf8feabca2c5aca27191
aphimuk <66010914024@msu.ac.th>
2026-02-04 16:38:11 +0700

    ฟ

--- h/index.php
+++ h/index.php
@@ -1,12 +1,6 @@
 <?php
 	session_start();
-	
-	unset($_SESSION['aid']) ;
-	unset($_SESSION['aname']) ;
-	
-		echo"<script>" ;
-		echo "window.location='index2.php';";
-		echo "</script>";
+    
 ?>
 
 <!doctype html>
@@ -29,16 +23,27 @@ Password<input type="password" name="apwd" required><br>
 <?php
 if(isset($_POST['Summit'])){
 	include_once("connectdb.php");
-	$sql ="SELECT * FROM admin WHERE a_username='{$_POST['auser']}' AND a_password='{$_POST['apwd']}'LIMIT 1 ";
-	$rs = mysqli_query($conn,$sql);
+    
+    
+    $username = mysqli_real_escape_string($conn, $_POST['auser']);
+    $password = mysqli_real_escape_string($conn, $_POST['apwd']);
+
+	$sql = "SELECT * FROM admin WHERE a_username='{$username}' AND a_password='{$password}' LIMIT 1 ";
+	$rs = mysqli_query($conn, $sql);
 	$num = mysqli_num_rows($rs);
 	
-	
 	if($num == 1){
-        $data = mysqli_num_rows($rs);
+        
+        $data = mysqli_fetch_array($rs); 
+        
         $_SESSION['aid'] = $data['a_id'];
-        $_SESSION['aname'] = $data['a_name'];
-    }else{
+        $_SESSION['aname'] = $data['a_name']; 
+        
+        
+        echo "<script>";
+        echo "window.location='index2.php';";
+        echo "</script>";
+    } else {
         echo "<script>";
         echo "alert('Username หรือ Password ไม่ถูกต้อง');";
         echo "</script>";
--- h/index2.php
+++ h/index2.php
@@ -12,13 +12,13 @@
 
 <h1> เข้าสู่ระบบหลังบ้าน - อภิมุข </h1>
 
-<?php echo "แอดมิน: ". $_SESSION['a_name']; ?> <br>
+<?php echo "แอดมิน: ". $_SESSION['aname']; ?> <br>
 
 <ul>
 	<a href="products.php"><li>จัดการสินค้า</li></a>
 	<a href="orders.php"><li>จัดการออเดอร์</li></a>
 	<a href="customers.php"><li>จัดการลูกค้า</li></a>
-	<a href="logout.php"><li>ออกจากระบบ</li></a>
+	<a href="logout.php"><li>ออกจากระบบ</li></a> 
 </ul>
 </body>
 </html>
