41e6d042711a39ca43aa73c4e2514b8c3b68a858
aphimuk <66010914024@msu.ac.th>
2026-02-04 15:25:08 +0700

    A

--- h/index.php
+++ h/index.php
@@ -15,5 +15,23 @@ Password<input type="password" name="apwd" required><br>
 <button type="submit" name="Summit">LOGIN</button>
 </form>
 
+<?php
+if(isset($_POST['Summit'])){
+	include_once("connectdb.php");
+	$sql ="SELECT * FROM admin WHERE a_username='{$_POST['auser']}' AND a_password='{$_POST['apwd']}'LIMIT 1 ";
+	$rs = mysqli_query($conn,$sql);
+	$num = mysqli_num_rows($rs);
+	
+	echo $num;
+	/*if($num == 1){
+		
+	}else{
+		echo "<script>";
+		echo"alert('Username หรือ Password ไม่ถูกต้อง')";
+		echo"</script>";
+	}*/
+}
+?>
+
 </body>
 </html>
