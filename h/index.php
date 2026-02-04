e4bd7f93eaaae64eacf9a95819503d524dcc28cf
aphimuk <66010914024@msu.ac.th>
2026-02-04 14:38:46 +0700

    a

--- h/index.php
+++ h/index.php
@@ -9,5 +9,11 @@
 
 <h1> อภิมุข แสงดอกไม้(แฟร้งค์)</h1>
 
+<form method="post" action="">
+Username<input type="text" name="auser" autofocus required><br>
+Password<input type="password" name="apwd" required><br>
+<button type="submit" name="Summit">LOGIN</button>
+</form>
+
 </body>
 </html>
