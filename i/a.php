8ad8ae5f6c4345080536bdbcb6af15ff802c881a
Aphimuk <66010914024@msu.ac.th>
2026-02-17 18:28:38 +0700

    a

--- i/a.php
+++ i/a.php
@@ -6,6 +6,28 @@
 </head>
 
 <h1> งานi-- อภิมุข แสงดอกไม้(แฟร้งค์) <h1>
+
+
+
+<table border="1">
+    <tr>
+        <th>รหัสภาค</th>
+        <th>ชื่อภาค</th>
+    </tr>
+<?php
+include_once("connectdb.php");
+$sql = "SELECT * FROM 'regions'";
+$rs = mysqli_query($conn,$sql);
+while ($data = mysqli_fetch_array($rs)){
+?>
+    <tr>
+        <td><?php echo $data['r_id']; ?></td> 
+        <td><?php echo $data['r_name']; ?></td> 
+    </tr>
+
+<?php } ?>
+</table>    
+
 <body>
 </body>
 </html>
