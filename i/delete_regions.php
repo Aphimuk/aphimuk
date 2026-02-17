75d680a6b78ab773ae5d70650837af27e9b39ac7
Aphimuk <66010914024@msu.ac.th>
2026-02-17 19:28:15 +0700

    a

--- i/delete_regions.php
+++ i/delete_regions.php
@@ -1,14 +1,14 @@
 <meta charset="utf-8">
 <?php
 include_once("connectdb.php");
-$id = $GET[$id];
+$id = $_GET['id'];
 
 $sql = "DELETE FROM regions WHERE r_id='{$id}'";
 mysqli_query($conn,$sql) or die ("ลบข้อมูลไม่ได้")
 
-echo "<scipt>";
+echo "<script>";
 echo "windoe.location='a.php'";
-echo "</scipt>";
+echo "</script>";
 
 ?>
 
