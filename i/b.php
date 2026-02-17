20c7672a108f5ea4cc4f7a6ed6d23a42470b2e3d
Aphimuk <66010914024@msu.ac.th>
2026-02-17 19:54:48 +0700

    a

---
+++ i/b.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>อภิมุข แสงดอกไม้</title>
+</head>
+
+<h1> งานi-- อภิมุข แสงดอกไม้(แฟร้งค์) <h1>
+
+<form method="post" action="" >
+	ชื่อภาค <input type="text" name="rname" autofocus required>
+    <button type="submit" name="Submit">บันทึก</button>	
+</form><br><br>
+
+<?php
+if(isset($_POST['Submit'])){
+	include_once("connectdb.php");
+	$rname = $_POST['rname'];
+	$sql2 = "INSERT INTO regions (p_id, p_name) VALUES (NULL, '{$rname}')";
+	mysqli_query($conn, $sql2) or die ("เพิ่มข้อมูลไม่ได้");
+}
+?>
+
+
+<table border="1">
+	<tr>
+    	<th>รหัสภาค</th>
+        <th>ชื่อภาค</th>
+        <th>ลบ</th>
+    </tr>
+<?php
+include_once("connectdb.php");
+$sql = "SELECT * FROM regions";
+$rs = mysqli_query($conn, $sql);
+ while ($data = mysqli_fetch_array($rs)){
+?>   
+    <tr>
+    	<td><?php echo $data['p_id'] ; ?></td>
+        <td><?php echo $data['p_name'] ;?></td>
+        <td width="80" align="center"><a href="delete_regions.php?id=<?php echo $data['p_id']; ?>" onClick="return confirm('d1');"><img src="img/1.jpg" width="50"></a></td>
+    </tr>
+<?php } ?>
+</table>
+
+</body>
+</html>
+
+<?php
+mysqli_close($conn);
+?>
