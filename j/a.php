9b045d135bdc5890aabf7d2bfd3410ce5e5ae302
aphimuk <66010914024@msu.ac.th>
2026-02-18 13:36:54 +0700

    a

---
+++ j/a.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>อภิมุข แสงดอกไม้(แฟร้งค์)</title>
+</head>
+
+<body>
+<h1>66010914024 อภิมุข แสงดอกไม้(แฟร้งค์)</h1>
+
+</body>
+</html>
